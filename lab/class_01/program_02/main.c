98d0906e65e956a33efb2dd968fbe38df3f18e01
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-12 01:36:06 +0530

    f8x

--- lab/class_01/program_02/main.c
+++ lab/class_01/program_02/main.c
@@ -1,26 +1,30 @@
 #include <math.h>
 #include <stdio.h>
 
-void getRoots(int a, int b, int c) {
-    int D = sqrt(b * b - 4 * a * c);
-    if (D >= 0) {
-        int x1 = (-b + D) / (2 * a);
-        int x2 = (-b - D) / (2 * a);
-        printf("Roots: %d, %d", x1, x2);
+void getRoots(double a, double b, double c) {
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant >= 0) {
+        double D = sqrt(discriminant);
+        double x1 = (-b + D) / (2 * a);
+        double x2 = (-b - D) / (2 * a);
+        printf("Roots: %.2f, %.2f", x1, x2);
     } else {
         printf("Real roots don't exist for this equation.");
     }
 }
 
 int main() {
-    int a, b, c;
+    double a, b, c;
     printf("For a quadratic equation: ax^2 + bx + c = 0,\n");
     printf("Give the value of \'a\' : ");
-    scanf("%d", &a);
+    scanf("%lf", &a);
     printf("Give the value of \'b\' : ");
-    scanf("%d", &b);
+    scanf("%lf", &b);
     printf("Give the value of \'c\' : ");
-    scanf("%d", &c);
+    scanf("%lf", &c);
 
     getRoots(a, b, c);
+
+    return 0;
 }
