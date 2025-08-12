093351d0fc064e513e71741155ad335f896b5f8f
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-12 12:47:43 +0530

    p4

--- lab/class_02/program_04/main.c
+++ lab/class_02/program_04/main.c
@@ -2,3 +2,24 @@
 Create struct Op { const char *name; int (*fn)(int,int); }. Build an array of
 operations (add, sub, mul, max) and call them via pointers. Print results.
 */
+
+#include <stdio.h>
+
+struct Op {
+    const char *name;
+    int (*fn)(int, int);
+};
+
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
+int mul(int a, int b) { return a * b; }
+int max(int a, int b) { return a > b ? a : b; }
+
+int main() {
+    struct Op ops[] = {{"add", add}, {"sub", sub}, {"mul", mul}, {"max", max}};
+    int x = 10, y = 5;
+    for (int i = 0; i < 4; i++) {
+        printf("%s(%d, %d) = %d\n", ops[i].name, x, y, ops[i].fn(x, y));
+    }
+    return 0;
+}
