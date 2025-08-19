b14328e6637dc0be3a7133dde661515692974cd0
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-19 10:09:42 +0530

    x

---
+++ lab/class_03/program_01/main.c
@@ -0,0 +1,42 @@
+1. Student Records Management
+
+    Create a program to store student
+    records(name, roll number, marks) in a file.
+
+Provide options to:
+
+Add a new record
+
+Display all records
+
+Search for a student by roll number
+
+Update marks of a student
+
+
+2. Word Frequency Counter
+
+Read a text file and count the frequency of each word.
+
+Save the results in another file in the format:
+
+word1 → frequency
+word2 → frequency
+
+3. Merge Two Files
+
+Take two text files as input and merge their contents into a third file.
+
+Add line numbers while writing into the new file.
+
+4. Copy File with Case Conversion
+
+Read contents of a file and copy them into another file.
+
+While copying:
+
+Convert all lowercase letters to uppercase.
+
+Convert all uppercase letters to lowercase.
+
+Dr. Sahana Das
