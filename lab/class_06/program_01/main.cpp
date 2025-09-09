7cf571dc4bf41bc564f768804bda93e87b348eef
whatisaProCoder <debnathpritam2005@gmail.com>
2025-09-09 10:09:27 +0530

    p1

---
+++ lab/class_06/program_01/main.cpp
@@ -0,0 +1,27 @@
+// Use friend function to calculate the average of two numbers.
+
+#include <iostream>
+using namespace std;
+
+class Numbers {
+ private:
+  float num1, num2;
+
+ public:
+  Numbers(float a, float b) {
+    num1 = a;
+    num2 = b;
+  }
+
+  friend float calculateAverage(Numbers n);
+};
+
+float calculateAverage(Numbers n) { return (n.num1 + n.num2) / 2; }
+
+int main() {
+  Numbers obj(10.5, 20.5);
+
+  cout << "Average = " << calculateAverage(obj) << endl;
+
+  return 0;
+}
