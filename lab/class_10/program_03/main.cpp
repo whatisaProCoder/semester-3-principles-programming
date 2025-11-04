45a9c37ffee253f149ace3df0c1ab8041d992c4b
whatisaProCoder <debnathpritam2005@gmail.com>
2025-11-04 11:31:18 +0530

    p3

--- lab/class_10/program_03/main.cpp
+++ lab/class_10/program_03/main.cpp
@@ -17,3 +17,78 @@ constructor's initializer list will need to initialize its origin member.
 
 In main(), create a Rectangle object and call its display() method.
 */
+
+#include <iostream>
+using namespace std;
+
+class Point {
+ private:
+  int x;
+  int y;
+
+ public:
+  Point(int x_val, int y_val) {
+    x = x_val;
+    y = y_val;
+  }
+  Point() {
+    x = 0;
+    y = 0;
+  }
+  void get_coords() { cout << "Point : (" << x << "," << y << ")" << endl; }
+};
+
+class Shape {
+ protected:
+  Point origin;
+
+ public:
+  Shape(int x, int y) { origin = Point(x, y); }
+};
+
+class Rectangle : public Shape {
+ private:
+  int width;
+  int height;
+
+ public:
+  Rectangle(int x, int y, int w, int h) : Shape(x, y) {
+    width = w;
+    height = h;
+  }
+  void display() {
+    Shape::origin.get_coords();
+    cout << "Width : " << width << endl;
+    cout << "Height : " << height << endl;
+  }
+};
+
+int main() {
+  int x, y, w, h;
+
+  cout << "Enter details...." << endl;
+  cout << "x : ";
+  cin >> x;
+  cout << "y : ";
+  cin >> y;
+  cout << "w : ";
+  cin >> w;
+  cout << "h : ";
+  cin >> h;
+
+  Rectangle r(x, y, w, h);
+  cout << "Printing details...." << endl;
+  r.display();
+}
+
+/* OUTPUT
+Enter details....
+x : 1
+y : 2
+w : 3
+h : 4
+Printing details....
+Point : (1,2)
+Width : 3
+Height : 4
+*/
