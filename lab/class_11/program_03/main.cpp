4ce6dae8793f05bef2c48a4bae1abc6edb049a45
whatisaProCoder <debnathpritam2005@gmail.com>
2026-01-06 15:01:59 +0530

    done

--- lab/class_11/program_03/main.cpp
+++ lab/class_11/program_03/main.cpp
@@ -3,4 +3,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {}
+int main() {
+  int a, b;
+
+  cout << "Enter a and b :" << endl;
+
+  cin >> a >> b;
+
+  try {
+    if (b == 0) {
+      throw runtime_error("Division by zero");
+    }
+    int c = a / b;
+    cout << "Result of division : " << c << endl;
+  } catch (const runtime_error& e) {
+    cerr << "Caught exception : " << e.what();
+  }
+}
+
+/*
+Enter a and b :
+1 0
+Caught exception : Division by zero
+*/
